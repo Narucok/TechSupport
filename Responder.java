806df73889c4ce30859a7d0ae4641996364de1c8
Naruto Sitanggang <162136955+Narucok@users.noreply.github.com>
2024-10-09 22:40:40 +0700

    Update Responder.java

--- Responder.java
+++ Responder.java
@@ -1,92 +1,130 @@
-import java.util.Random;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Random;
+
 public class Responder
 {
-    // instance variables - replace the example below with your own
-    Random answerNum;
-    private String[] answers;
-    private HashMap<String, Integer>keywords;
-    int allAnswers = 22;
+    // Used to map key words to responses.
+    private HashMap<String, String> responseMap;
+    // Default responses to use if we don't recognise a word.
+    private ArrayList<String> defaultResponses;
+    private Random randomGenerator;
 
     /**
-     * Constructor for objects of class Responder
+     * Construct a Responder
      */
     public Responder()
     {
-        // initialise instance variables
-        answers();
-        keywordslist();
+        responseMap = new HashMap<String, String>();
+        defaultResponses = new ArrayList<String>();
+        fillResponseMap();
+        fillDefaultResponses();
+        randomGenerator = new Random();
     }
-  
-    public String generateResponse(String input)
+
+    /**
+     * Generate a response from a given set of input words.
+     * 
+     * @param words  A set of words entered by the user
+     * @return       A string that should be displayed as the response
+     */
+    public String generateResponse(HashSet<String> words)
     {
-        // put your code here
-        answerNum = new Random();
-        int ai = 0;
-        int lastAnswer = 0;
-        boolean match = false;
-        
-        String[] splitInput = input.split("[^a-zA-Z]");
-        
-        for(int i = 0; i < splitInput.length && !match; i++)
-        
-        {
-            if(keywords.containsKey(splitInput[i]))
-            {
-                match = true;
-                ai = keywords.get(splitInput[i]);
-            }
-        }    
-        if(!match)
-        {
-            while(ai == lastAnswer){
-                ai = answerNum.nextInt(10);
+        for (String word : words) {
+            String response = responseMap.get(word);
+            if(response != null) {
+                return response;
             }
         }
-        lastAnswer = ai;
-        return answers[ai];
+        
+        // If we get here, none of the words from the input line was recognized.
+        // In this case we pick one of our default responses (what we say when
+        // we cannot think of anything else to say...)
+        return pickDefaultResponse();
     }
-    
-    private void answers()
+
+    /**
+     * Enter all the known keywords and their associated responses
+     * into our response map.
+     */
+private void fillResponseMap()
+{
+    responseMap.put("crash", 
+                    "Yah, ini tidak pernah crash di sistem kami. Mungkin ada sesuatu\n" +
+                    "yang berhubungan dengan sistem Anda. Ceritakan lebih lanjut tentang konfigurasi Anda.");
+    responseMap.put("crashes", 
+                    "Yah, ini tidak pernah crash di sistem kami. Mungkin ada sesuatu\n" +
+                    "yang berhubungan dengan sistem Anda. Ceritakan lebih lanjut tentang konfigurasi Anda.");
+    responseMap.put("slow", 
+                    "Saya rasa ini berkaitan dengan perangkat keras Anda. Meningkatkan prosesor Anda\n" +
+                    "harusnya menyelesaikan semua masalah kinerja. Apakah ada masalah dengan\n" +
+                    "perangkat lunak kami?");
+    responseMap.put("performance", 
+                    "Kinerja cukup memadai di semua pengujian kami. Apakah Anda menjalankan\n" +
+                    "proses lain di latar belakang?");
+    responseMap.put("bug", 
+                    "Anda tahu, semua perangkat lunak memiliki beberapa bug. Tapi para insinyur perangkat lunak\n" +
+                    "kami bekerja sangat keras untuk memperbaikinya. Bisakah Anda menjelaskan masalahnya lebih lanjut?");
+    responseMap.put("buggy", 
+                    "Anda tahu, semua perangkat lunak memiliki beberapa bug. Tapi para insinyur perangkat lunak\n" +
+                    "kami bekerja sangat keras untuk memperbaikinya. Bisakah Anda menjelaskan masalahnya lebih lanjut?");
+    responseMap.put("windows", 
+                    "Ini adalah bug yang diketahui berkaitan dengan sistem operasi Windows. Silakan\n" +
+                    "laporkan ke Microsoft. Tidak ada yang bisa kami lakukan terkait hal ini.");
+    responseMap.put("mac", 
+                    "Ini adalah bug yang diketahui berkaitan dengan sistem operasi Mac. Silakan\n" +
+                    "laporkan ke Apple. Tidak ada yang bisa kami lakukan terkait hal ini.");
+    responseMap.put("expensive", 
+                    "Biaya produk kami cukup kompetitif. Apakah Anda sudah membandingkan fitur-fitur kami?");
+    responseMap.put("installation", 
+                    "Proses instalasi sebenarnya cukup sederhana. Kami memiliki banyak wizard\n" +
+                    "yang akan melakukan semua pekerjaan untuk Anda. Apakah Anda sudah membaca\n" +
+                    "petunjuk instalasi?");
+    responseMap.put("memory", 
+                    "Jika Anda membaca persyaratan sistem dengan teliti, Anda akan melihat bahwa\n" +
+                    "persyaratan memori yang disebutkan adalah 1,5 gigabyte. Anda benar-benar perlu\n" +
+                    "meningkatkan memori Anda. Ada hal lain yang ingin Anda ketahui?");
+    responseMap.put("linux", 
+                    "Kami sangat serius dalam mendukung Linux. Tapi ada beberapa masalah.\n" +
+                    "Sebagian besar terkait dengan versi glibc yang tidak kompatibel. Bisakah Anda\n" +
+                    "menjelaskannya dengan lebih detail?");
+    responseMap.put("bluej", 
+                    "Ahhh, BlueJ, ya. Kami sudah mencoba membeli perusahaan mereka sejak lama, tetapi\n" +
+                    "mereka tidak mau menjualnya... Orang-orangnya cukup keras kepala. Tidak ada yang bisa\n" +
+                    "kami lakukan terkait hal itu.");
+}
+
+
+    /**
+     * Build up a list of default responses from which we can pick one
+     * if we don't know what else to say.
+     */
+private void fillDefaultResponses()
+{
+    defaultResponses.add("Itu terdengar aneh. Bisakah Anda menjelaskan masalah itu dengan lebih detail?");
+    defaultResponses.add("Tidak ada pelanggan lain yang pernah mengeluh tentang ini sebelumnya. \n" +
+                         "Bagaimana konfigurasi sistem Anda?");
+    defaultResponses.add("Itu terdengar menarik. Ceritakan lebih lanjut...");
+    defaultResponses.add("Saya butuh sedikit lebih banyak informasi tentang itu.");
+    defaultResponses.add("Sudahkah Anda memeriksa bahwa Anda tidak memiliki konflik dll?");
+    defaultResponses.add("Itu dijelaskan di manual. Apakah Anda sudah membaca manual?");
+    defaultResponses.add("Deskripsi Anda agak samar. Apakah ada ahli di sana \n" +
+                         "yang dapat menjelaskan ini dengan lebih rinci?");
+    defaultResponses.add("Itu bukan bug, itu fitur!");
+    defaultResponses.add("Bisakah Anda menjelaskan lebih lanjut?");
+}
+
+    /**
+     * Randomly select and return one of the default responses.
+     * @return     A random default response
+     */
+    private String pickDefaultResponse()
     {
-        answers = new String[allAnswers];
-        answers[0] = "Did you ask google?";
-        answers[1] = "Did you turn it off and on?";
-        answers[2] = "Keep talking, i'll get myself a coffee.";
-        answers[3] = "ohhh my gosh...";
-        answers[4] = "42";
-        answers[5] = "i have no clue.";
-        answers[6] = "have a break, have a kitkat";
-        answers[7] = "wenn's mal wieder länger dauert...";
-        answers[8] = "loading 31%";
-        answers[9] = "talk to the hand.";
-        answers[10] = "java, java, java";
-        answers[11] = "Computer, Rechner, Ordenador";
-        answers[12] = "apple, apple, apple, ...";
-        answers[13] = "Microsoft, Winzigweich, pico fofo";
-        answers[14] = "the answer to life, the universe and everything is 42";
-        answers[15] = "Mac, mac, mack, muck, mick...";
-        answers[16] = "Aaahhh... we're all gonna die...";
-        answers[17] = "this might take a li'l while.";
-        answers[18] = "printing is not good for the enviroment.";
-        answers[19] = "which distribution?";
-        answers[20] = "did you try rooting it?";
-    }
-    
-    private void keywordslist()
-    {
-        keywords = new HashMap<String, Integer>();
-        keywords.put("java", 10);
-        keywords.put("computer", 11);
-        keywords.put("ios", 12);
-        keywords.put("windows", 13);
-        keywords.put("life", 14);
-        keywords.put("mac", 15);
-        keywords.put("crashed", 16);
-        keywords.put("loading", 17);
-        keywords.put("print", 18);
-        keywords.put("linux", 19);
-        keywords.put("android", 20);
-        keywords.put("problem", 2);
+        // Pick a random number for the index in the default response list.
+        // The number will be between 0 (inclusive) and the size of the list (exclusive).
+        int index = randomGenerator.nextInt(defaultResponses.size());
+        return defaultResponses.get(index);
     }
 }
